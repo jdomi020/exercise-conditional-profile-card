865e3ef9f973efd39fb507121957887f1da110c8
Jose Dominguez <141463676+jdomi020@users.noreply.github.com>
2023-11-07 02:29:51 +0000

    Still showing null as part of dropdown, not sure if im supposed to take that off, but actual profile generated isnt showing null

--- src/js/app.js
+++ src/js/app.js
@@ -51,12 +51,22 @@ function render(variables = {}) {
           <h1>${variables.name ? variables.name : "First Name"}&nbsp
           ${variables.lastName ? variables.lastName : "Last Name"}</h1>
           <h2>${variables.role ? variables.role : "Role"}</h2>
-          <h3>${city} ${variables.country ? variables.country : "Country"}&nbsp ${variables.city ? variables.city : "Miami"}</h3>
+          <h3>${variables.city ? variables.city : "Miami"} &nbsp ${
+    variables.country ? variables.country : "Country"
+  } </h3>
           <ul class=${variables.socialMediaPosition}>
-            <li><a href="https://twitter.com/ ${variables.twitter ? variables.twitter : "4geeksacademy" }"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/${variables.github ? variables.github : "4geeksacademy" }"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/${variables.linkedin ? variables.linkedin : "4geeksacademy" }"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/${variables.instagram ? variables.instagram : "4geeksacademy" }"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="https://twitter.com/ ${
+              variables.twitter ? variables.twitter : "4geeksacademy"
+            }"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${
+              variables.github ? variables.github : "4geeksacademy"
+            }"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/school/${
+              variables.linkedin ? variables.linkedin : "4geeksacademy"
+            }"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${
+              variables.instagram ? variables.instagram : "4geeksacademy"
+            }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
